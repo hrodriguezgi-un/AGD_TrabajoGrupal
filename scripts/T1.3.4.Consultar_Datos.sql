5f7abd97c47e44068133da0e1d3a9c539132176f
Harvey Rodriguez <hrodriguezgi@gmail.com>
2021-05-10 20:45:22 -0500

    Update new query
    Update new query

--- scripts/T1.3.4.Consultar_Datos.sql
+++ scripts/T1.3.4.Consultar_Datos.sql
@@ -10,6 +10,7 @@ FROM
 GROUP BY 
 	ANIO;
 	
+
 -- 2. TOTAL DE REGISTROS POR ENTIDAD ADMINISTRADORA DE SALUD EAS
 SELECT 
 	E.descripcion AS NOMBRE_EAS
@@ -23,6 +24,7 @@ GROUP BY
 ORDER BY 
 	SUM(A.total_atenciones) DESC 
 LIMIT 10;
+
 
 -- 3. TOTAL ATENCIONES POR IPS Y SERVICIO PARA HOMBRES MAYORES DE 50 AÑOS
 SELECT 
@@ -41,3 +43,21 @@ GROUP BY
 ORDER BY 
 	SUM(A.total_atenciones) DESC 
 LIMIT 10;
+
+
+-- 4. 2 PRIMEROS DIAGNÓSTICO Y CAUSA EXTERNA CON MAYOR FRECUENCIA
+SELECT 
+	 D.diagnostico
+	,CE.causa_externa
+	,SUM(A.total_atenciones) AS total_atenciones 
+FROM 
+	atencion as A
+	LEFT JOIN diagnostico AS D ON 
+		A.cod_dx_ppal_egreso = D.cod
+	LEFT JOIN causa_externa  AS CE ON 
+		A.cod_causa_externa = CE.cod
+GROUP BY 
+	1,2
+ORDER BY 
+	3 DESC
+LIMIT 2;
